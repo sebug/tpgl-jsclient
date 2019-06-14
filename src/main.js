f1df1d21e0a5a61910b97c33e2fae23e20e870e0
sebug <sebastian.gfeller@gmail.com>
2019-06-14 13:11:37 +0200

    Adding timeout

--- src/main.js
+++ src/main.js
@@ -1,19 +1,46 @@
 const key = new URLSearchParams(location.search).get('key');
 console.log('key is ' + key);
 
+const shortTimeout = 2 * 1000;
+
+// calls a promise callback with a timeout mechanism
+// inspired by https://davidwalsh.name/fetch-timeout
+function timeout(milliseconds,callback) {
+    let didTimeOut = false;
+    const to = setTimeout(function() {
+        didTimeOut = true;
+        reject(new Error('Request timed out'));
+    }, milliseconds);
+
+    callback().then(response => {
+	clearTimeout(to);
+	if (!didTimeOut) {
+	    resolve(response);
+	}
+    }).catch(err => {
+	if (didTimeOut) {
+	    return;
+	}
+	reject(err);
+    });
+}
+
 function getStops() {
     return fetch('https://tpgl-proxy.azurewebsites.net/api/GetStops?key=' + key, {
 	mode: 'cors'
     }).then(response => response.json());
 }
 
-getStops().then(stopResult => {
+timeout(shortTimeout, getStops).then(stopResult => {
     const stops = stopResult.stops;
     const stopNames = stops.map(s => s.stopName);
     const stopElements = stopNames.map(s => '<option value="' + s + '"></option>');
     const stopInnerHtml = stopElements.join(' ');
     const stopDataList = document.querySelector('#stops');
     stopDataList.innerHTML = stopInnerHtml;
+}).catch(err => {
+    console.log('timed out I guess');
+    console.log(err);
 });
 
 if ('serviceWorker' in navigator) {

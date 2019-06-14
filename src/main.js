d68f5236bfb9b2a14446cac7aed55b80f78923e6
sebug <sebastian.gfeller@gmail.com>
2019-06-14 12:51:33 +0200

    Adding cors call for getting stops

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
+const key = new URLSearchParams(location.search).get('key');
+console.log('key is ' + key);
+
+function getStops() {
+    return fetch('https://tpgl-proxy.azurewebsites.net/api/GetStops?key=' + key, {
+	mode: 'cors'
+    }).then(response => response.json());
+}
+
+getStops().then(stops => {
+    console.log(stops);
+});
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js').then(function (reg) {
 	console.log('Registration succeeded, scope is ' + reg.scope);
